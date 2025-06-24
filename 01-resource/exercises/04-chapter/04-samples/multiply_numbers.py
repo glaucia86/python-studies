b4787fe41e700601ec878c9e259137d68bf8a2f7
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2025-06-24 13:13:32 -0300

    feat: add script to list and print multiples of 3 from 3 to 30 using range and for loop

---
+++ 01-resource/exercises/04-chapter/04-samples/multiply_numbers.py
@@ -0,0 +1,5 @@
+# Faça uma lista com os múltiplos de 3 de 3 a 30 usando range() e um laço for para exibi-los.
+
+numeros_multiplos = list(range(3, 31, 3))
+for multiplo in numeros_multiplos:
+    print(multiplo)
