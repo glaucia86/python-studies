452894172160258f9d8ce2c79c49e0100111e645
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2025-06-24 13:26:36 -0300

    feat: add script to display the first three, middle three, and last three items from a pizza list

---
+++ 01-resource/exercises/04-chapter/04-samples/pizza_02.py
@@ -0,0 +1,18 @@
+# Pegue um dos programas anteriores e adicione linhas para:
+# * Mostrar os três primeiros itens da lista com uma mensagem.
+# * Mostrar três itens do meio da lista.
+# * Mostrar os três últimos itens da lista.
+
+pizzas = ['calabresa', 'quatro queijos', 'camarão', 'pepperoni', 'mussarela', 'frango com catupiry', 'vegetariana', 'portuguesa']
+
+print("Os três primeiros itens da lista de pizzas são...:")
+print(pizzas[:3])
+print()
+
+print("Três itens do meio da lista de pizzas são...:")
+print(pizzas[3:6])
+print()
+
+print("Os três últimos itens da lista de pizzas são...:")
+print(pizzas[-3:])
+
