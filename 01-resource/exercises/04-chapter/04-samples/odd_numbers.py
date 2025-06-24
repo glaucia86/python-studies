65a691f356780283d3de17852373fac4632480c7
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2025-06-24 13:11:21 -0300

    feat: add script to generate and print odd numbers from 1 to 20 using range and for loop

---
+++ 01-resource/exercises/04-chapter/04-samples/odd_numbers.py
@@ -0,0 +1,6 @@
+# Use a função range() com três argumentos para gerar uma lista de números 
+# ímpares de 1 a 20. Use um laço for para imprimir cada número.
+
+impares = list(range(1, 21, 2))
+for impar in impares:
+    print(impar)
